0c1f69c9d41a4cba9fdb195d2b8875e17274e388
midhunrajk <midhunrajk009@gmail.com>
2023-08-19 14:21:13 +0530

    first page completed

--- src/components/FirstPage.tsx
+++ src/components/FirstPage.tsx
@@ -1,10 +1,54 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Button, Typography, TextField } from "@mui/material";
 
 const FirstPage = () => {
-    return(
-        <div>
-            First Page
-        </div>
-    )
-}
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const navigate = useNavigate();
+
+    const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newVal = e.target.value;
+        setPhone(newVal)
+        if(newVal.length !== 10) {
+            setErrorMessage('Enter a valid 10-digit number');
+        }
+        if(!/^\d+$/.test(newVal)) {
+            setErrorMessage('Input must contain only digits');
+        } 
+        else{
+            setErrorMessage('');
+        }
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        localStorage.setItem("userDetails", JSON.stringify({ name, phone, email }));
+        navigate("/second1");
+    };
+
+    return (
+        <Box display="flex">
+            <Box width="100%">
+                <img style={{ width: "60vw", height: "97.2vh" }} src="./assets/login.jpg" alt="login image" />
+            </Box>
+            <Box display="flex" flexDirection="column" alignItems="center" gap="10px" justifyContent="center" width="100%">
+                <Typography variant="h4">User Login</Typography>
+                <form onSubmit={handleSubmit} style={{ width: "60%" }}>
+                    <TextField required sx={{ marginBottom: "10px" }} label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
+                    <TextField required sx={{ marginBottom: "10px" }} label="Phone" error={errorMessage !== ''} value={phone} onChange={handlePhoneChange} fullWidth />
+                    {errorMessage && <Typography color="error">{errorMessage}</Typography>}
+                    <TextField required sx={{ marginBottom: "10px" }} label="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} fullWidth />
+                    <Button type="submit" variant="contained" color="primary" fullWidth>
+                        Submit
+                    </Button>
+                </form>
+            </Box>
+        </Box>
+    );
+};
 
 export default FirstPage;
